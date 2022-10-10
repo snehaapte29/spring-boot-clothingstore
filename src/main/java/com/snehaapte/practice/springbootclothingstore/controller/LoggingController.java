fcde81b5bf0cfde6311ee83d57fcb62fc4b3924b
Sneha Apte <sneha29@gmail.com>
2022-10-11 04:55:21 +0530

    renamed and updated files and packages

--- src/main/java/com/snehaapte/practice/springbootclothingstore/controller/LoggingController.java
+++ src/main/java/com/snehaapte/practice/springbootclothingstore/controller/LoggingController.java
@@ -1,4 +1,4 @@
-package com.snehaapte.practice.springstarterproject.controller;
+package com.snehaapte.practice.springbootclothingstore.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
